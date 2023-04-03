2d3088d912b0ef5eadd69593d64bbe49c95409d6
GodsTheGuy <srijangod10@gmail.com>
2023-04-03 22:21:00 +0530

    Update before Unity 2023 Version

--- Wild Trucks/Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
+++ Wild Trucks/Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
@@ -36,9 +36,9 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = ""; // Filled in automatically
+        public const string ApplicationId = "536769999996"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "536769999996-bd6icpos8gkf2gpr4095p5747e4dn0g7.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
--- Wild Trucks/Assets/_Scripts/PlayFab/LoginManager.cs
+++ Wild Trucks/Assets/_Scripts/PlayFab/LoginManager.cs
@@ -1,18 +1,85 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using PlayFab;
+using PlayFab.ClientModels;
+using PlayFab.PfEditor.EditorModels;
+using Mono.Cecil.Cil;
+using PlayFabError = PlayFab.PlayFabError;
 
 public class LoginManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
     }
 
-    // Update is called once per frame
-    void Update()
+    internal void ProcessAuthentication(SignInStatus status)
     {
-        
+        if (status == SignInStatus.Success)
+        {
+            // Continue with Play Games Services
+            Debug.Log("Success");
+        }
+        else
+        {
+            Debug.Log(status);
+            // Disable your integration with Play Games Services or show a login button
+            // to ask users to sign-in. Clicking it should call
+            // PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication).
+        }
     }
+
+
+
+    public void OnSignInButtonClicked()
+    {
+        PlayGamesPlatform.Activate();
+
+        PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+
+        /*
+        Social.localUser.Authenticate((bool success) => {
+
+            if (success)
+            {
+                Debug.Log("Signed In");
+
+                //var serverAuthCode;
+                PlayGamesPlatform.Instance.RequestServerSideAccess(false, code =>
+                {
+                    var serverAuthCode = code;
+
+                    PlayFabClientAPI.LoginWithGoogleAccount(new LoginWithGoogleAccountRequest()
+                    {
+                        TitleId = PlayFabSettings.TitleId,
+                        ServerAuthCode = serverAuthCode,
+                        CreateAccount = true
+                    }, (result) =>
+                    {
+                        Debug.Log("Signed In as " + result.PlayFabId);
+
+                    }, OnLoginWithGoogleAccountFailure);
+                });
+
+                
+            }
+            else
+            {
+                Debug.Log("Google Failed to Authorize your login");
+            }
+
+        });
+        */
+
+    }
+
+    private void OnLoginWithGoogleAccountFailure(PlayFabError error)
+    {
+        Debug.Log("PlayFab LoginWithGoogleAccount Failure: " + error.GenerateErrorReport());
+    }
+
+
 }
